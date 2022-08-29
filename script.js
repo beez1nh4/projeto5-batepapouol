4a3652b1ee4ab2046333eb00a70bc0a671c809b0
Melissa <melissahentona@usp.br>
2022-08-28 22:06:50 -0300

    mensagens add

--- script.js
+++ script.js
@@ -1,30 +1,83 @@
-let nomeDoUsuario = prompt('Qual o seu lindo nome?')
-let nomeDoUsuarioObj = {
-    name: nomeDoUsuario
-}
-console.log(nomeDoUsuarioObj)
-let enviarNome = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomeDoUsuarioObj)
+let ul = document.querySelector('.espacoMensagens');
+let nomeDoUsuario;
+let nomeDoUsuarioObj;
+let enviarNome;
 
+function nome(){
+        nomeDoUsuario = prompt('Qual o seu lindo nome?')
+        nomeDoUsuarioObj = {
+        name: nomeDoUsuario
+    }
+    enviarNome = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomeDoUsuarioObj)
+    enviarNome.then(sucesso);
+    enviarNome.catch(erro);
+}
+    
 function sucesso(){
     console.log('enviou!!!!');
 }
-function erro(){
+function erro(erro){
+    mandouNome = false;
+    while (erro.response.status == 400){
     nomeDoUsuario = prompt('Esse nome já está em uso. Escolha outro:');
     nomeDoUsuarioObj = {
         name: nomeDoUsuario
     }
-    //enviarNome = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomeDoUsuarioObj)
+    enviarNome = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomeDoUsuarioObj)
+    enviarNome.then(sucesso);
+}
 }
 
-enviarNome.then(sucesso);
+nome();
 enviarNome.catch(erro);
-
 function statusDoUsuario(){
     let enviarStatus = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nomeDoUsuarioObj)
 }
 
 setInterval(statusDoUsuario,5000);
 
-let mensagem = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+function mensagensDoServidor(){
+    let mensagem = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    mensagem.then(pegarMensagem)
+}
+mensagensDoServidor();
+
+function pegarMensagem(conteudo){
+    let elemento = conteudo.data;
+    //console.log(elemento[0].type)
+    mostraMensagem(elemento)
+}
 
 //type da mensagem for status, message ou private_message
+function mostraMensagem(elemento){
+    //console.log(elemento.length)
+    for (let i = 0; i < elemento.length; i++){
+        ul.innerHTML += `
+        <li class="${elemento[i].type}"> <span class="time">(${elemento[i].time})</span> <span class="bold">${elemento[i].from}</span> para <span class="bold">${elemento[i].to}:</span> ${elemento[i].text} </li>
+        `
+    }
+    let ultimaMensagem = ul.lastElementChild;
+    ultimaMensagem.scrollIntoView();
+}
+
+function enviarMensagemDigitada(){
+    let mensagemDigitada = document.querySelector('.campo')
+    let mensagemDigitadaObj = {
+        from: nomeDoUsuario,
+        to: "Todos", //ou pessoa para bônus
+        text: mensagemDigitada.value,
+        type: "message" // ou "private_message" para o bônus
+    }
+    mandarMsgServidor = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages',mensagemDigitadaObj)
+    mandarMsgServidor.then(envioSucesso)
+    mandarMsgServidor.catch(envioErro)
+}
+
+
+function envioErro(){
+    console.log("Não foi possível enviar a mensagem")
+}
+function envioSucesso(){
+    console.log('mensagem enviada!!!')
+    window.location.reload()
+}
